8d188d10d3cd20e45c4ebc74a6a666fce029348c
Athul Nath <tkathulnath@gmail.com>
2024-09-30 12:05:09 +0530

    front end commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,56 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screen/homescreen';
 import ProductsScreen from './screen/productscreen';
+import  Navbar from 'react-bootstrap/Navbar';
+import  Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { Store } from './store';
+import CartScreen from './screen/cartscreen';
+import SigninScreen from './screen/signinscreen';
 
 function App() {
+  const {state} = useContext(Store);
+  const {cart} = state;
   return (
     <>
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to='/'>Amazona</Link>
+        <Navbar bg='dark' variant='dark'>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>Shoppy.io</Navbar.Brand>
+            </LinkContainer>
+            <Nav className="me-auto">
+              <Link to='/cart' className='nav-link'>
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <Badge pill bg='danger'>
+                    {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
+                  </Badge>
+                )
+                }
+              </Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </header>
       <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductsScreen/>} />
-          <Route path="/" element={<HomeScreen/>} />
-        </Routes>
+        <Container className='mt-3'>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductsScreen />} />
+            <Route path="/cart" element={<CartScreen />}/>
+            <Route path="/signin" element={<SigninScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>All rights Reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
     </>
---
+++ client/src/components/product.js
@@ -0,0 +1,50 @@
+import Button  from "react-bootstrap/Button";
+import Card  from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Rating from "./rating";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../store";
+
+function Product (props){
+    const {product} = props;
+
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { 
+        cart: { cartItems },
+    } = state;
+
+    const addToCartHandler = async (item) =>{
+        const existItem = cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${item._id}`);
+        if (data.countInStock < quantity){
+            window.alert("Sorry. Product is out of Stock..");
+            return;
+        }
+        ctxDispatch({
+            type: 'CART_ADD_ITEM',
+            payload: {...item, quantity},
+        });
+    }
+    return (
+            <Card key={product.slug}>
+                <Link to={`/product/${product.slug}`}>
+                    <img src={product.image} className='card-img-top' alt={product.name} />
+                </Link>
+                <Card.Body>
+                    <Link to={`/product/${product.slug}`}>
+                        <Card.Title>{product.name}</Card.Title>
+                    </Link>
+                    <Rating rating ={product.rating} numReviews={product.numReviews}/>
+                    <Card.Text>₹ {product.price}</Card.Text>
+                    {product.countInStock === 0
+                    ? (<Button variant="light" disabled>Out of Stock</Button>
+                    ) : (
+                    <Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+                    )}
+                </Card.Body>
+            </Card>
+    )
+}
+export default Product;

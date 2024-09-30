46a7d3160fa5789ea86ace7b9dae459c2e3d780d
Athul Nath <tkathulnath@gmail.com>
2024-09-30 12:13:59 +0530

    back end commit

---
+++ server/data.js
@@ -0,0 +1,57 @@
+const data= {
+    products : [
+        {
+            //_id : '1',
+            name : 'Nike Slim shirt',
+            slug : 'nike-slim-shirt',
+            category : 'shirt',
+            image : '/images/shirt1.avif',//679px  829px
+            price : 500,
+            countInStock : 13,
+            brand : 'Nike',
+            rating : 3.8,
+            numReviews : 14,
+            description : 'high quality shirt'
+        },
+        {
+            //_id : '2',
+            name : 'Addidas Slim shirt',
+            slug : 'addidas-slim-shirt',
+            category : 'shirt',
+            image : '/images/shirt2.avif',
+            price : 600,
+            countInStock : 10,
+            brand : 'Addidas',
+            rating : 4.5,
+            numReviews : 18,
+            description : 'high quality shirt'
+        },
+        {
+            //_id : '3',
+            name : 'Nike Fit pant',
+            slug : 'nike-fit-pant',
+            category : 'pant',
+            image : '/images/pant1.webp',
+            price : 1100,
+            countInStock : 0,
+            brand : 'Nike',
+            rating : 3.0,
+            numReviews : 11,
+            description : 'high quality pant'
+        },
+        {
+            //_id : '4',
+            name : 'Addidas Fit pant',
+            slug : 'addidas-fit-pant',
+            category : 'pant',
+            image : '/images/pant2.avif',
+            price : 1200,
+            countInStock : 5,
+            brand : 'Addidas',
+            rating : 3.8,
+            numReviews : 15,
+            description : 'high quality pant'
+        },
+    ]
+}
+export default data;
